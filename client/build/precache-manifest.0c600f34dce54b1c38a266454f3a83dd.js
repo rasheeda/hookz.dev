59757167a3362e04921e2f7cda9a84d4080c1649
Rasheeda Mandeeya <ryehuza@gmail.com>
2020-05-24 15:32:48 +0200

    Building for release due to memory timeouts building on prod

---
+++ client/build/precache-manifest.0c600f34dce54b1c38a266454f3a83dd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "230e41ac8d8c0078bcbb472f07d8b93b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f4be971acc8e41e409f6",
+    "url": "/static/css/2.051905ae.chunk.css"
+  },
+  {
+    "revision": "4453e3f9601402fd1986",
+    "url": "/static/css/main.fa84bcd6.chunk.css"
+  },
+  {
+    "revision": "f4be971acc8e41e409f6",
+    "url": "/static/js/2.5586cac8.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.5586cac8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4453e3f9601402fd1986",
+    "url": "/static/js/main.65548362.chunk.js"
+  },
+  {
+    "revision": "8bf69297da9cf43fc69a",
+    "url": "/static/js/runtime-main.da9796fe.js"
+  }
+]);
